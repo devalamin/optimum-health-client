2eead668d337b8bdf46317adf6e0ad284113fcfb
devalamin <alamin.hasibmd2017@gmail.com>
2022-11-08 11:33:59 +0600

    review

--- src/Components/Pages/Services/ServiceDetails.js
+++ src/Components/Pages/Services/ServiceDetails.js
@@ -1,11 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import ReviewRow from './UserReview/ReviewRow';
 
 const ServiceDetails = () => {
-    const { service_name } = useLoaderData();
+
+    const [reviews, setReviews] = useState([])
+    const { service_name, price, description, _id, img } = useLoaderData();
+
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/reviews?service_name=${service_name}`)
+            .then(res => res.json())
+            .then(data => {
+                setReviews(data)
+            })
+
+    }, [service_name])
+
+
     return (
-        <div>
-            About this service {service_name}
+        <div className='sm:my-10'>
+            <div className="hero min-h-screen" style={{ backgroundImage: `url(${img})` }}>
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-start text-neutral-content">
+                    <div className="">
+                        <h1 className="mb-5 sm:text-5xl sm:font-bold">{service_name}</h1>
+                        <p className="mb-5">{description}</p>
+                        <button className="btn btn-warning ">$ {price}</button>
+                        <button className="btn btn-wide sm:ml-10 mt-4 bg-teal-800">Get An Appointment</button>
+                    </div>
+                </div>
+            </div>
+            <div className='my-10'>
+                <h1 className='my-5 text-center text-2xl font-bold text-teal-900'>User's reviews-</h1>
+
+                <div>
+                    <div className="overflow-x-auto w-full">
+                        <table className="table w-full">
+
+                            <thead>
+                                <tr>
+
+                                    <th>Name</th>
+                                    <th>Comments</th>
+                                    <th>Phone</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    reviews.map(singleReview => <ReviewRow
+                                        key={singleReview._id}
+                                        singleReview={singleReview}
+
+                                    ></ReviewRow>)
+                                }
+
+                            </tbody>
+
+
+
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/Components/Pages/Services/UserReview/ReviewRow.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const ReviewRow = ({ singleReview }) => {
+    const { service_name, user_name, email, review, img, phone } = singleReview;
+    return (
+        <tr>
+            <td>
+                <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                        <div className=" w-12 h-12">
+                            <img src={img} alt="Avatar Tailwind CSS Component" />
+                        </div>
+                    </div>
+                    <div>
+                        <div className="font-bold">{user_name}</div>
+
+
+                    </div>
+                </div>
+            </td>
+            <td>
+                <p>{review}</p>
+                <br />
+
+            </td>
+            <td>{phone}</td>
+            <th>
+                <button className="btn btn-ghost lowercase btn-xs">{email}</button>
+            </th>
+        </tr>
+    );
+};
+
+export default ReviewRow;
